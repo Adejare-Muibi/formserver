1dd815278e54aa695aeac3e98f5219e926f8735c
Toyyib Lawal <toyibe25@gmail.com>
2024-05-02 16:50:28 +0100

    bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
-import Image from "next/image";
-import Navbar from "@/components/landing-page/Navbar";
-import Hero from "@/components/landing-page/Hero"
-import Build from "@/components/landing-page/Build"
-import How from "@/components/landing-page/How"
-import Squash from "@/components/landing-page/Squash"
-import Create from "@/components/landing-page/Create"
-import Comp from "@/components/landing-page/Comp"
-import Footer from "@/components/landing-page/Footer"
+import Image from 'next/image';
+import Navbar from '@/components/landing-page/Navbar';
+import Hero from '@/components/landing-page/Hero';
+import Build from '@/components/landing-page/Build';
+import How from '@/components/landing-page/How';
+import Squash from '@/components/landing-page/Squash';
+import Create from '@/components/landing-page/Create';
+import Comp from '@/components/landing-page/Comp';
+import Footer from '@/components/landing-page/Footer';
 
-const page = () => {
-  return (
-    <section>
-      <Hero/>
-      <Build/>
-      <How/>
-      <Squash/>
-      <Create/>
-      <Comp/>
-    </section>
-  );
-}
+const Page = () => {
+	return (
+		<section>
+			<Hero />
+			<Build />
+			<How />
+			<Squash />
+			<Create />
+			<Comp />
+		</section>
+	);
+};
 
-export default page
+export default Page;
