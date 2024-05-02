6c5156e33d653276f8d3ddc3e6ca28c5197a0a48
Toyyib Lawal <toyibe25@gmail.com>
2024-05-02 16:56:38 +0100

    bug fixes

--- src/components/landing-page/Hero.jsx
+++ src/components/landing-page/Hero.jsx
@@ -1,57 +1,63 @@
-"use client";
-import Link from "next/link";
-import React, { useState } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+'use client';
+import Link from 'next/link';
+import React, {useState} from 'react';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {vscDarkPlus} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const Hero = () => {
-  const [isCopy, setIsCopy] = useState(false);
-  const codeString = `<form action="https://formserver.io/f/{form_id}" method="post">
+	const [isCopy, setIsCopy] = useState(false);
+	const codeString = `<form action="https://formserver.io/f/{form_id}" method="post">
     < label for= "email" > Your Email</label >
       <input name="Email" id="email" type="email">
         <button type="submit">Submit</button>
       </form>`;
 
-  const handleCopyCode = () => {
-    navigator.clipboard.writeText(codeString);
-    setIsCopy(!isCopy);
+	const handleCopyCode = () => {
+		navigator.clipboard.writeText(codeString);
+		setIsCopy(!isCopy);
 
-    setTimeout(() => {
-      setIsCopy(false)
-    }, 2000);
-  };
-  
+		setTimeout(() => {
+			setIsCopy(false);
+		}, 2000);
+	};
 
-  return (
-    <div className="flex w-full flex-col justify-center items-center py-10 gap-12 max-md:sticky">
-      <div className="flex flex-col items-center gap-4">
-        <h1 className="text-[50px] font-bold max-md:text-center max-md:text-[39px] max-md:w-[80vw]">
-          Easy Form Endpoints for any developer
-        </h1>
-        <p className="text-center text-[20px] max-md:w-[80vw] w-[60vw]">
-          Connect your Form to our form Endpoints and we we'll email you the submissions. No PHP, Javascript or any backend code required.
-        </p>
-        <Link
-          href={"/register"}
-          className="px-7 py-4 bg-[#c02dc1] text-white border-none rounded-[5px] "
-        >
-          Get Started
-        </Link>
-      </div>
-      <div className="flex max-md:w-[90%] flex-col relative items-center gap-4">
-          <SyntaxHighlighter
-          customStyle={{ padding: "40px", borderRadius: "20px", width:"100%"}}
-          language="javascript"
-          style={vscDarkPlus}
-        >
-
-          {codeString}
-        </SyntaxHighlighter>
-        <button className="bg-[#384951] absolute top-3 py-1 text-sm px-4 rounded right-2 text-white text-opacity-40" onClick={handleCopyCode}>{isCopy ? "Copied!" : "Copy"}</button>
-        <p className="w-[30vw] text-center text-[18px] max-md:w-[90vw]">But you have to write your own CSS codes for your forms </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex w-full flex-col justify-center items-center py-10 gap-12 max-md:sticky">
+			<div className="flex flex-col items-center gap-4">
+				<h1 className="text-[50px] font-bold max-md:text-center max-md:text-[39px] max-md:w-[80vw]">
+					Easy Form Endpoints for any developer
+				</h1>
+				<p className="text-center text-[20px] max-md:w-[80vw] w-[60vw]">
+					Connect your Form to our form Endpoints and we we&apos;ll email you
+					the submissions. No PHP, Javascript or any backend code required.
+				</p>
+				<Link
+					href={'/register'}
+					className="px-7 py-4 bg-[#c02dc1] text-white border-none rounded-[5px] "
+				>
+					Get Started
+				</Link>
+			</div>
+			<div className="flex max-md:w-[90%] flex-col relative items-center gap-4">
+				<SyntaxHighlighter
+					customStyle={{padding: '40px', borderRadius: '20px', width: '100%'}}
+					language="javascript"
+					style={vscDarkPlus}
+				>
+					{codeString}
+				</SyntaxHighlighter>
+				<button
+					className="bg-[#384951] absolute top-3 py-1 text-sm px-4 rounded right-2 text-white text-opacity-40"
+					onClick={handleCopyCode}
+				>
+					{isCopy ? 'Copied!' : 'Copy'}
+				</button>
+				<p className="w-[30vw] text-center text-[18px] max-md:w-[90vw]">
+					But you have to write your own CSS codes for your forms{' '}
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
--- src/components/landing-page/How.jsx
+++ src/components/landing-page/How.jsx
@@ -1,88 +1,129 @@
-import React from 'react'
-import Image from 'next/image'
-import { MdOutlineSecurity } from "react-icons/md";
-import { MdMarkEmailUnread } from "react-icons/md";
-import { RiInboxArchiveFill } from "react-icons/ri";
-import { FaShareAlt } from "react-icons/fa";
+import React from 'react';
+import Image from 'next/image';
+import {MdOutlineSecurity} from 'react-icons/md';
+import {MdMarkEmailUnread} from 'react-icons/md';
+import {RiInboxArchiveFill} from 'react-icons/ri';
+import {FaShareAlt} from 'react-icons/fa';
 
 const How = () => {
-  return (
-    <div className='py-[20px] flex items-center w-full justify-around max-md:justify-center max-md:flex-col'>
-        <div>
-            <Image className='w-[] max-md:hidden rounded-[15px]' src={"/front.avif"} height={60} width={400} />
-        </div>
-        <div className='flex flex-col gap-[70px] max-md:items-center'>
-            <div className='max-md:hidden'>
-                <h1 className='font-bold text-[40px]'>How it works</h1>
-                <p className='w-[42vw]'>Here's what happens after your form is submitted using Javascript forms, HTML forms or <span className='font-bold text-[17px]'>Formbutton</span>.</p>
-            </div>
-            <div className='max-md:flex max-md:flex-col max-md:items-center lg:hidden gap-[42px]'>
-                <div className='flex flex-col items-center'>
-                    <h1 className='font-bold text-[40px]'>How it works</h1>
-                <p className='w-[75vw] max-md:text-center'>Here's what happens after your form is submitted using Javascript forms, HTML forms or <span className='font-bold text-[17px]'>Formbutton</span>.</p>
-                </div>
-                <div>
-                    <Image className='w-[] lg:hidden rounded-[15px]' src={"/front.avif"} height={140} width={250} />
-                </div>
-            </div>
-            <div className='max-md:items-center max-md:flex max-md:flex-col max-md:gap-[22px] items-center'>
-                <div className='flex flex-col gap-[45px] max-md:items-center'>
-                    <div className='flex gap-[12px]'>
-                        <div>
-                            <MdOutlineSecurity className='text-[30px]'/>
-                        </div>
-                        <div>
-                            <h1 className='font-bold text-[20px] max-md:text-[18px] max-md:w-[55vw]'>Spam blocked, data validated</h1>
-                            <p className='max-md:w-[60vw]'>We validate your data server-side and use machine learning to protect you from spam</p>
-                        </div>
-                    </div>
-                    <div className='flex gap-[12px]'>
-                        <div>
-                            <MdMarkEmailUnread className='text-[30px]'/>
-                        </div>
-                        <div>
-                            <h1 className='font-bold text-[20px] max-md:text-[18px] max-md:w-[55vw]'>Email notifications and auto-responses are sent</h1>
-                            <p className='max-md:w-[60vw]'>Our email templates are fully customizable</p>
-                        </div>
-                    </div>
-                    <div className='flex gap-[12px]'>
-                        <div>
-                            <RiInboxArchiveFill className='text-[30px]'/>
-                        </div>
-                        <div>
-                            <h1 className='font-bold text-[20px] max-md:text-[18px] max-md:w-[55vw]'>Submissions are saved to the Formspree Inbox</h1>
-                            <p className='max-md:w-[60vw]'>View submissions, see daily analytics reports, and export to CSV or JSON</p>
-                        </div>
-                    </div>
-                    <div className='flex gap-[12px]'>
-                        <div>
-                            <FaShareAlt className='text-[30px]'/>
-                        </div>
-                        <div>
-                            <h1 className='font-bold text-[20px] max-md:text-[18px] max-md:w-[55vw]'>3rd party integrations are triggered</h1>
-                            <p className='w-[42vw] max-md:w-[60vw]'>With our direct integrations, we'll get your data where it needs to be - no Zapier required</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex items-center'>
-                    <div className='flex gap-[40px] items-center flex-col'>
-                        <Image className='w-[10vw]' width={80} height={80} src={"/airtable.png"}/>
-                        <MdMarkEmailUnread className='text-[30px]'/>   
-                    </div>
-                    <div className='flex gap-[40px] items-center flex-col'>
-                        <Image width={80} height={80} src={"/convertkit.png"}/>
-                        <Image width={80} height={80} src={"/freshdesk.png"}/>
-                    </div>
-                    <div className='flex gap-[40px] items-center flex-col'>
-                        <Image width={80} height={80} src={"/discord.png"}/>
-                        <Image width={80} height={80} src={"/gladly.png"}/>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-    </div>
-  )
-}
+	return (
+		<div className="py-[20px] flex items-center w-full justify-around max-md:justify-center max-md:flex-col">
+			<div>
+				<Image
+					className="w-[] max-md:hidden rounded-[15px]"
+					src={'/front.avif'}
+					height={60}
+					width={400}
+				/>
+			</div>
+			<div className="flex flex-col gap-[70px] max-md:items-center">
+				<div className="max-md:hidden">
+					<h1 className="font-bold text-[40px]">How it works</h1>
+					<p className="w-[42vw]">
+						Here&apos;s what happens after your form is submitted using
+						Javascript forms, HTML forms or{' '}
+						<span className="font-bold text-[17px]">Formbutton</span>.
+					</p>
+				</div>
+				<div className="max-md:flex max-md:flex-col max-md:items-center lg:hidden gap-[42px]">
+					<div className="flex flex-col items-center">
+						<h1 className="font-bold text-[40px]">How it works</h1>
+						<p className="w-[75vw] max-md:text-center">
+							Here&apos;s what happens after your form is submitted using
+							Javascript forms, HTML forms or{' '}
+							<span className="font-bold text-[17px]">Formbutton</span>.
+						</p>
+					</div>
+					<div>
+						<Image
+							className="w-[] lg:hidden rounded-[15px]"
+							src={'/front.avif'}
+							height={140}
+							width={250}
+						/>
+					</div>
+				</div>
+				<div className="max-md:items-center max-md:flex max-md:flex-col max-md:gap-[22px] items-center">
+					<div className="flex flex-col gap-[45px] max-md:items-center">
+						<div className="flex gap-[12px]">
+							<div>
+								<MdOutlineSecurity className="text-[30px]" />
+							</div>
+							<div>
+								<h1 className="font-bold text-[20px] max-md:text-[18px] max-md:w-[55vw]">
+									Spam blocked, data validated
+								</h1>
+								<p className="max-md:w-[60vw]">
+									We validate your data server-side and use machine learning to
+									protect you from spam
+								</p>
+							</div>
+						</div>
+						<div className="flex gap-[12px]">
+							<div>
+								<MdMarkEmailUnread className="text-[30px]" />
+							</div>
+							<div>
+								<h1 className="font-bold text-[20px] max-md:text-[18px] max-md:w-[55vw]">
+									Email notifications and auto-responses are sent
+								</h1>
+								<p className="max-md:w-[60vw]">
+									Our email templates are fully customizable
+								</p>
+							</div>
+						</div>
+						<div className="flex gap-[12px]">
+							<div>
+								<RiInboxArchiveFill className="text-[30px]" />
+							</div>
+							<div>
+								<h1 className="font-bold text-[20px] max-md:text-[18px] max-md:w-[55vw]">
+									Submissions are saved to the Formspree Inbox
+								</h1>
+								<p className="max-md:w-[60vw]">
+									View submissions, see daily analytics reports, and export to
+									CSV or JSON
+								</p>
+							</div>
+						</div>
+						<div className="flex gap-[12px]">
+							<div>
+								<FaShareAlt className="text-[30px]" />
+							</div>
+							<div>
+								<h1 className="font-bold text-[20px] max-md:text-[18px] max-md:w-[55vw]">
+									3rd party integrations are triggered
+								</h1>
+								<p className="w-[42vw] max-md:w-[60vw]">
+									With our direct integrations, we&apos;ll get your data where
+									it needs to be - no Zapier required
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="flex items-center">
+						<div className="flex gap-[40px] items-center flex-col">
+							<Image
+								className="w-[10vw]"
+								width={80}
+								height={80}
+								src={'/airtable.png'}
+							/>
+							<MdMarkEmailUnread className="text-[30px]" />
+						</div>
+						<div className="flex gap-[40px] items-center flex-col">
+							<Image width={80} height={80} src={'/convertkit.png'} />
+							<Image width={80} height={80} src={'/freshdesk.png'} />
+						</div>
+						<div className="flex gap-[40px] items-center flex-col">
+							<Image width={80} height={80} src={'/discord.png'} />
+							<Image width={80} height={80} src={'/gladly.png'} />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default How
+export default How;
