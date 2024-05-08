38d06b4164410cc96b091c4bb7ac85612848dc41
Toyyib Lawal <toyibe25@gmail.com>
2024-05-08 18:09:04 +0100

    bugs fixes

---
+++ src/app/error/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorForm = () => {
+	return (
+		<main className="h-[85vh] flex flex-col justify-center items-center">
+			<h2 className="text-bold text-2xl text-red-500">
+				Sorry, this form is no longer available for submission
+			</h2>
+		</main>
+	);
+};
+
+export default ErrorForm;
---
+++ src/app/submit/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FormSubmit = () => {
+	return (
+		<main className="h-[85vh] flex flex-col justify-center items-center">
+			<h2 className="text-bold text-2xl text-green-500">Congratulations!!</h2>
+			<h2 className="text-bold text-2xl text-green-500">
+				Your form has submitted successfully
+			</h2>
+		</main>
+	);
+};
+
+export default FormSubmit;
